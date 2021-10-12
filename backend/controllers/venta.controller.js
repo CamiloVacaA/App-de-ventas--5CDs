b4ab0d380c581ea8fa4e80ec797770f69e3b29a0
Camilo <bryanbyn2@gmail.com>
2021-10-11 19:57:43 -0500

    Venta controller

---
+++ backend/controllers/venta.controller.js
@@ -0,0 +1,29 @@
+const venta = require('../models/venta')
+
+async function saveVenta(req, res) {
+    const { num_venta, id_usuario, fecha} = req.body
+   
+    const newVen = new venta({
+        num_venta,
+        id_usuario,
+        fecha
+    }) 
+    let ven = await newVen.save()
+    res.status(200).json(ven)
+}
+
+async function getAll(req, res){
+    let result = await venta.find();
+    res.status(200).json(result)
+}
+
+async function search(req, res){
+    let result = await venta.find(req.query)
+    res.status(200).json(result)
+}
+
+module.exports = {
+    saveVenta,
+    getAll,
+    search
+}
--- backend/models/venta.js
+++ backend/models/venta.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const saleSchema = mongoose.Schema({
     num_venta: String,
     id_usuario: String,
-    date: Date,
+    fecha: Date,
 },
 {
   versionKey: false
