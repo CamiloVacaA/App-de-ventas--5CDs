2b8071d1f956c5065fcc2fdb57afc642b24cf15a
gamezquitac <gamezquitacun@gmail.com>
2021-10-11 19:49:48 -0500

    corrección

--- backend/routes/sale_details.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-
-const controller = require('../controllers/sale_details.controller')
-
-router.get('/', controller.getAll)
-router.post('/', controller.saveProduct)
-router.get('/search', controller.search)
-
-module.exports = router
