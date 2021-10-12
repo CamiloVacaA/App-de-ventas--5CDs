6ceacd9f437152e9a97f472572c865fb6537bd94
Camilo <bryanbyn2@gmail.com>
2021-10-11 19:22:50 -0500

    Usuario

---
+++ backend/models/usuario.js
@@ -0,0 +1,25 @@
+/*import  Mongoose from "mongoose";
+
+
+const userSchema = new Mongoose.Schema({
+        id_usuario: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    nombre_usuario: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    correo: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    rol: String,
+    estado: String
+}); 
+
+export default Mongoose.model("user", userSchema);
+*/ 
