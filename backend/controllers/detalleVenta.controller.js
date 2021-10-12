f30407fe238171a5a83e6f599772023e1c0c88b6
gamezquitac <gamezquitacun@gmail.com>
2021-10-11 19:24:31 -0500

    detalle venta controller creation

---
+++ backend/controllers/detalleVenta.controller.js
@@ -0,0 +1,30 @@
+const detalleVenta = require('../models/detalle_venta')
+
+async function saveProduct(req, res) {
+    const { nombre, descripcion, valor_unitario, estado} = req.body
+   
+    const newProd = new Product({
+        nombre,
+        descripcion,
+        valor_unitario,
+        estado
+    }) 
+    let prod = await newProd.save()
+    res.status(200).json(prod)
+}
+
+async function getAll(req, res){
+    let result = await Product.find();
+    res.status(200).json(result)
+}
+
+async function search(req, res){
+    let result = await Product.find(req.query)
+    res.status(200).json(result)
+}
+
+module.exports = {
+    saveProduct,
+    getAll,
+    search
+}
